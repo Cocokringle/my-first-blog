5af7fc73c174e2d8207040b2dc77cecaf06af7fe
Cocokringle <cocokringle.1@gmail.com>
2016-11-26 18:50:33 +0200

    KITTENS ATTACK!

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
-
+from django.conf import settings
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
@@ -8,3 +8,10 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
