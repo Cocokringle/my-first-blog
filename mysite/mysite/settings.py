d7477005bd6025189dca1bd968d2fc0efca8afa2
Cocokringle <cocokringle.1@gmail.com>
2016-11-26 18:43:50 +0200

    added images in templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,4 +103,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
